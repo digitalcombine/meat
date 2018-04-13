f43d19787a7e12ae591d47436c3f3a64ac680b10
Ron Wills <ron.rwsoft@gmail.com>
2018-04-13 17:41:05 -0600

    Fixed copyright to GPL.

--- src/memory.cpp
+++ src/memory.cpp
@@ -1,6 +1,20 @@
 /*                                                                  -*- c++ -*-
- * Copyright ©
- * All rights reserved
+* Copyright (c) 2017 Ron R Wills <ron.rwsoft@gmail.com>
+*
+* This file is part of Meat.
+*
+* Meat is free software: you can redistribute it and/or modify
+* it under the terms of the GNU General Public License as published by
+* the Free Software Foundation, either version 3 of the License, or
+* (at your option) any later version.
+*
+* Meat is distributed in the hope that it will be useful,
+* but WITHOUT ANY WARRANTY; without even the implied warranty of
+* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+* GNU General Public License for more details.
+*
+* You should have received a copy of the GNU General Public License
+* along with Meat.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <meat/memory.h>
